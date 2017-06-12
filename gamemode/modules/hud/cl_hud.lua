131390420377ae77cc8c7a4d22331c5f9bdb2dcc
MisterTakaashi <soapmctravich@gmail.com>
2017-06-12 21:44:29 +0200

    Ajout test HUD

---
+++ gamemode/modules/hud/cl_hud.lua
@@ -0,0 +1,1 @@
+print("JE SUIS LE CLIENT !")
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -6,8 +6,9 @@ GM.Website = "puresystem.eu"
 -- Variable globale pour stocker un max de trucs
 PureRP = PureRP or {}
 
+-- On dérive du gamemode Sandbox
+DeriveGamemode("sandbox")
+
 -- On inclue la config et la config spécifique du scenario
 include("config/sh_config.lua")
 include("config/scenarios/sh_" .. PureRP.Config.Scenario .. ".lua")
-
-DeriveGamemode("sandbox")
